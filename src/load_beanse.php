92a93de152dd1e7d48ce5f220a7bb698173fa82c
sarath <sarath.hacks@gmail.com>
2014-05-17 09:31:50 +0530

    Updated

--- src/load_beanse.php
+++ src/load_beanse.php
@@ -2,5 +2,13 @@
 include 'connection.php';
 
 #Load Single row
-//$contact=R::load('contacts',4);
-//var_dump($contact);
+/*$contact=R::load('contacts',4);
+var_dump($contact);*/
+
+#To load a series of beans use: *Failed Doc
+/* $contact=R::loadAll('contacts');
+var_dump($contact); */
+
+#Finding (If you do not know the ID of a bean, you can search for beans like this:)
+$contact=R::findAll('contacts');
+var_dump($contact);
