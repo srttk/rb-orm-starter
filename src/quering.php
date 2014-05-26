d1e9d2b05d9fe2d65c8abfa325a7f0469dd13208
sarath <sarath.hacks@gmail.com>
2014-05-26 20:34:33 +0530

    Added quering.php

---
+++ src/quering.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Quering .
+	Execute row SQL Statement.
+
+
+*/
+	/* R::exec() */
+	R::exec( 'UPDATE user SET name="Sarath" WHERE id=93' );
+
+	/* R::getAll */
+
+	R::getAll("SELECT * FROM contacts");
